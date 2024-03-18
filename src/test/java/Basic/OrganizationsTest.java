978d308f684a53090397cabb08346b5d22168f53
Rutujakale123 <151734111+Rutujakale123@users.noreply.github.com>
2024-03-18 18:10:11 +0530

    Update OrganizationsTest.java

--- src/test/java/Basic/OrganizationsTest.java
+++ src/test/java/Basic/OrganizationsTest.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import CommonUtils.PropertyFileUtil;
-import CommonUtils.WebDriverUtil;
 
 public class OrganizationsTest {
 		
